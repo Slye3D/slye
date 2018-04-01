e78dd6c9ea02938c23de750fa8b334615ac6c345
Parsa Ghadimi <qti3eqti3e@gmail.com>
2018-04-01 20:13:43 +0430

    Config electron and build

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import url from "url";
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, "renderer/app.html"),
+    protocol: "file:",
+    slashes: true
+  }));
+}
+
+app.on("ready", createWindow);
