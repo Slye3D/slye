9527e3415d48604654b17f90f4d68a8ab6296dab
Parsa Ghadimi <me@qti3e.com>
2018-06-06 16:52:01 +0430

    math: Implement setDistance function

--- player.tsx
+++ player.tsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import * as types from "./types";
 
+// Constants
+export const NEAR = 1;
+export const FAR = 2000000;
+export const FOV = 70;
+export const GR = 1.61803398875;
+
 export interface PlayerProps {
   onClose(): void;
   steps: types.Step[]
 }
 
 export class Player extends Component<PlayerProps, {}> {
+  // Distance between camera and step.
+  distance: number;
+  playerDiv: HTMLElement;
+
   keydown(event) {
     if (event.keyCode === 9 || 
       (event.keyCode >= 32 && event.keyCode <= 34) ||
@@ -68,6 +78,32 @@ export class Player extends Component<PlayerProps, {}> {
     return false;
   }
 
+  // TODO Write documentations for this function. :/
+  setDistance() {
+    // TODO Get WMax and HMax from steps.
+    // WMax is maximum width we have in all steps.
+    const WMax = 800;
+    // Like WMax
+    const HMax = 200;
+    // D=Display=<w:width, h: height>
+    const Dw = this.playerDiv.offsetWidth;
+    const Dh = this.playerDiv.offsetHeight;
+    const FD = HMax / 2;
+    const DH = WMax / 2;
+    const OB = NEAR;
+    const AB = Math.tan((FOV / 2) * Math.PI / 180)
+    const BI = AB * Dw / Dh;
+    let BJ = GR * BI / (1 + GR);
+    const BJoverBE = DH / FD;
+    let BE = BJ / BJoverBE;
+    if (BE > (AB - 30)) {
+      BE = GR * AB / (1 + GR);
+      BJ = BE * BJoverBE;
+    }
+    const OD = OB * FD / BE;
+    this.distance = OD;
+  }
+
   handleClose() {
     this.props.onClose();
   }
@@ -80,9 +116,14 @@ export class Player extends Component<PlayerProps, {}> {
     // TODO Go to previous step
   }
 
+  handleRef = (player) => {
+    this.playerDiv = player;
+    this.setDistance();
+  }
+
   render() {
     return (
-      <div id="player" />
+      <div id="player" ref={ this.handleRef } />
     );
   }
 }
