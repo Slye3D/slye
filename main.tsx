f1bd60a72b6ff9e3d13e8179360f6af8d1e53923
Parsa Ghadimi <me@qti3e.com>
2018-06-06 21:58:13 +0430

    Fix input binding bug

--- main.tsx
+++ main.tsx
@@ -11,33 +11,40 @@ interface StepProps {
 class Step extends Component<StepProps, {}> {
   handleTextChange = (event) => {
     const newStep: types.Step = { ...this.props.step };
-    newStep["text"] = event.currentTarget.value;
+    newStep.text = event.target.value;
     this.props.onChange(newStep);
   };
 
   handleVec3Change = (field: types.StepVec3Props, axis: types.Axis, e) => {
     const newStep: types.Step = { ...this.props.step };
-    newStep[field][axis] = Number(e.currentTarget.value);
+    newStep[field][axis] = e.target.value;
     this.props.onChange(newStep);
   }
 
   render() {
     return (
       <div className="step">
-        <input type="text" placeholder="Text..." onChange={ this.handleTextChange }/>
+        <input
+          type="text"
+          placeholder="Text..."
+          value={ this.props.step.text }
+          onChange={ this.handleTextChange }/>
         Position:
         <div className="position">
           <input
             type="number"
             placeholder="X"
+            value={ this.props.step.position.x }
             onChange={ this.handleVec3Change.bind(this, "position", "x") } />
           <input
             type="number"
             placeholder="Y"
+            value={ this.props.step.position.y }
             onChange={ this.handleVec3Change.bind(this, "position", "y") } />
           <input
             type="number"
             placeholder="Z"
+            value={ this.props.step.position.z }
             onChange={ this.handleVec3Change.bind(this, "position", "z") } />
         </div>
         Orientation:
@@ -45,14 +52,17 @@ class Step extends Component<StepProps, {}> {
           <input
             type="number"
             placeholder="X"
+            value={ this.props.step.orientation.x }
             onChange={ this.handleVec3Change.bind(this, "orientation", "x") } />
           <input
             type="number"
             placeholder="Y"
+            value={ this.props.step.orientation.y }
             onChange={ this.handleVec3Change.bind(this, "orientation", "y") } />
           <input
             type="number"
             placeholder="Z"
+            value={ this.props.step.orientation.z }
             onChange={ this.handleVec3Change.bind(this, "orientation", "z") } />
         </div>
       </div>
@@ -84,9 +94,10 @@ class App extends Component<{}, AppState> {
   }
 
   handleStepChange = (i: number, newStep: types.Step) => {
-    this.setState(s => {
-      s[i] = newStep;
-      return s;
+    const newSteps = [ ...this.state.steps ];
+    newSteps[i] = newStep;
+    this.setState({
+      steps: newSteps,
     });
   };
 
