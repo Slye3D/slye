f1480f541116103a7e724e4ff1105d5a95426841
Parsa Ghadimi <me@qti3e.com>
2018-06-10 22:27:41 +0430

    Setup React router

---
+++ main.tsx
@@ -0,0 +1,16 @@
+/**
+ *    _____ __
+ *   / ___// /_  _____
+ *   \__ \/ / / / / _ \
+ *  ___/ / / /_/ /  __/
+ * /____/_/\__, /\___/
+ *       /____/
+ *       Copyright 2018 Parsa Ghadimi. All Rights Reserved.
+ *       Licence: MIT License
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "./router";
+
+ReactDOM.render(<Router />, document.getElementById("root"));
---
+++ router.tsx
@@ -0,0 +1,25 @@
+/**
+ *    _____ __
+ *   / ___// /_  _____
+ *   \__ \/ / / / / _ \
+ *  ___/ / / /_/ /  __/
+ * /____/_/\__, /\___/
+ *       /____/
+ *       Copyright 2018 Parsa Ghadimi. All Rights Reserved.
+ *       Licence: MIT License
+ */
+
+import React, { Component } from "react";
+import { HashRouter, Route } from "react-router-dom";
+
+import { Editor } from "./editor";
+
+export class Router extends Component<{}, {}> {
+  render() {
+    return (
+      <HashRouter>
+        <Route path="/" component={ Editor } />
+      </HashRouter>
+    );
+  }
+}
